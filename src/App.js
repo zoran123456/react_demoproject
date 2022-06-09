88b78cff0ff5c2309cb14f596bc1c2db60e44017
Zoran Bosnjak <zoran0406@gmail.com>
2022-06-09 13:59:36 +0200

    Initialize Application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import PostsCollection from "./components/postsCollection/postsCollection";
+import { Navigate, Route, Routes } from "react-router-dom";
+import PostDetail from "./components/postDetail/postDetail";
+import { AppContext, createAppContextValue } from "./store/appContext";
+import { apiGetPostsComments } from "./services/postService";
 
 function App() {
+  const handleError = (error) => {
+    console.error("API ERROR", error);
+  };
+
+  const logComponentInitializatorPrefix = "Component Created: ";
+
+  const [items, setItems] = useState([]);
+
+  const [appContextValue, setAppContextValue] = useState(
+    // Current page, Rows per page, Row filter
+    createAppContextValue(1, 20, "")
+  );
+
+  // Component initialization
+  useEffect(() => {
+    const fetchData = async () => {
+      const itemsCombined = await apiGetPostsComments();
+
+      setItems(itemsCombined);
+    };
+
+    fetchData().catch((error) => handleError(error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <AppContext.Provider value={{ appContextValue, setAppContextValue }}>
+          <Routes>
+            <Route
+              path="/posts"
+              element={
+                <PostsCollection
+                  initializationPrefix={logComponentInitializatorPrefix}
+                  posts={items}
+                />
+              }
+            />
+            <Route
+              path="/post/:id"
+              element={
+                <PostDetail
+                  initializationPrefix={logComponentInitializatorPrefix}
+                />
+              }
+            />
+            <Route path="/" element={<Navigate to="/posts" />} />
+            <Route path="*" element={<h2>ĐE BI TI??!</h2>} />
+          </Routes>
+        </AppContext.Provider>
+      </main>
     </div>
   );
 }
