c6addd4f0e1e7c4cb6fad085b39352b5fb34e378
Zoran Bosnjak <zoran0406@gmail.com>
2022-06-09 13:54:42 +0200

    Add constants file

---
+++ src/constants/api.js
@@ -0,0 +1,5 @@
+const API_LOCATION = "https://jsonplaceholder.typicode.com";
+
+export const ApiPostsUrl = `${API_LOCATION}/posts`;
+export const ApiUsersUrl = `${API_LOCATION}/users`;
+export const ApiCommentsUrl = `${API_LOCATION}/comments`;
